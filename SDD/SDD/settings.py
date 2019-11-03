22e3a9fa7a4273e71c6dea00c7c67652177f4f3e
Hman7003 <Hman7003@gmail.com>
2019-11-03 15:52:40 -0500

    Update settings.py

--- SDD/SDD/settings.py
+++ SDD/SDD/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sponsor.apps.SponsorConfig',
     'schedule.apps.ScheduleConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
