959ae21b4fbab7d867be5e395c0b31942c089eef
Hman7003 <Hman7003@gmail.com>
2019-11-03 23:05:29 -0500

    Combined everything into 4 categories
    USE THIS BRANCH

--- SDD/schedule/models.py
+++ SDD/schedule/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-class Showings(models.Model):
-    showing_pkey = models.AutoField(primary_key=True)
-    date = models.DateField('last time seen')
-    time = models.TimeField('time shown')
-    attendance = models.IntegerField()
-    showing_id = models.IntegerField()
-
-    movie = models.ForeignKey(
-        Movie,
-        on_delete = models.CASCADE
-    )
-
 class Movie(models.Model):
 
     imdb_id = models.CharField(max_length=32, blank=False, null=False)
@@ -28,3 +16,15 @@ class Movie(models.Model):
     trailer_url = models.CharField(max_length=2048)
     rotten_tomatoes_rating = models.CharField(max_length=32)
     image_path = models.CharField(max_length=2048)
+
+class Showings(models.Model):
+    showing_pkey = models.AutoField(primary_key=True)
+    date = models.DateField('last time seen')
+    time = models.TimeField('time shown')
+    attendance = models.IntegerField()
+    showing_id = models.IntegerField()
+
+    movie = models.ForeignKey(
+        Movie,
+        on_delete = models.CASCADE
+    )
